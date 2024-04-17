65167d63a500c7ed235a2635522b74f90102e281
Juan Garcia Bartolome <juag@ciklum.com>
2024-04-17 09:47:36 +0200

    First Docker Version Added

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: true,
     port: 5123,
     proxy: {
       '^/api/.*': {
-        target: 'http://localhost:5321',
+        target: 'http://serial_monitor_asp:5321',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         ws: true
